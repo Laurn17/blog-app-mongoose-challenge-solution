1fbee607944e4087b2640c4a1290db90b23e54bd
Lauren Morrow <morrowlauren17@gmail.com>
2018-10-10 17:44:35 -0700

    ?

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ function runServer(databaseUrl, port = PORT) {
       if (err) {
         return reject(err);
       }
-      server = app.listen(PORT, () => {
+      server = app.listen(port, () => {
         console.log(`Your app is listening on port ${port}`);
         resolve();
       })
